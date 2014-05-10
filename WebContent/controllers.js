2abfc1f71d5a5bc973ae0f816a4daf1eda8ef57b
Paul Robinson <niffydroid@gmail.com>
2014-05-10 14:50:23 +0100

    Fixed JSON bug.
    contacts.json (supplied for the task) was formatted differently than required. I believe the json was formated to be included into the code.  I have created contacts.json.fix which is correctly formatted to be read in as a file.
    The original file when being accessed throwed the error "Unexpected token n at Object.parse (native)" this was because the name was not enclosed in any quotes, it had to be enclosed in double quotes. While the value was enclosed in only single quotes, it had to be doubled quotes.

--- WebContent/controllers.js
+++ WebContent/controllers.js
@@ -3,12 +3,11 @@ var contactControllers = angular.module('contactControllers', []);
 
 contactControllers.controller('ContactListCtrl', ['$scope', '$http',
 	function($scope, $http){
-		
-		$http.get('contacts.json').success(function(data){
+		$http.get('contacts.json.fix').success(function(data){
 			$scope.contacts = data;
 		});
 		
-		//$scope.orderPop = 'name';
+		$scope.orderPop = 'name';
 
 }]);
 
