b21fd69d712cd7e720c5fba47766cf2c01912d9a
Paul Robinson <niffydroid@gmail.com>
2014-05-11 13:37:27 +0100

    Cleaned up the JS code and added comments(along with thoughts)

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,7 +1,16 @@
 'use strict';
-
+/*
+ * Creating the an app module called contactApp, 
+ * it has dependencies of ngRoute for the route provider and contactControllers
+ * which controls specific parts of the app.
+ */
 var contactApp = angular.module('contactApp', ['ngRoute', 'contactControllers']);
 
+/*
+ * Config the app, we require the routeProvider so inject it!
+ * This basically looks at the url and depending on the path it loads
+ * the template and controller to construct the related view.
+ */
 contactApp.config(['$routeProvider',
 	function($routeProvider){
 	$routeProvider.
@@ -9,6 +18,9 @@ contactApp.config(['$routeProvider',
 				templateUrl: 'contactList.html',
 				controller: 'ContactListCtrl'
 			}).
+			/*
+			 * When the contact ID is in the url change the view (template and controller)
+			 */
 			when('/contacts/:contactID',{
 				templateUrl: 'contactDetail.html',
 				controller: 'ContactDetailCtrl'
--- WebContent/controllers.js
+++ WebContent/controllers.js
@@ -1,58 +1,102 @@
 'use strict';
+/*
+ * Create a new module called contactControllers which will contain the controllers
+ */
 var contactControllers = angular.module('contactControllers', []);
 
+/*
+ * A factory which will store the data for use by both controllers.
+ * We require this, in my view, as its a simple way of sharing data without
+ * doing things like broadcasting or storing in the root scope.
+ * It contains the contactList, the next ID number to use and if the data
+ * has been collected.
+ * 
+ * I need to look at collecting the data in the factory without any prompting
+ * from a controller to go and collect it.
+ * Although a service might be what is really required here.
+ * I need to look at the difference between factory, service and provider.
+ */
 contactControllers.factory('Data', function($http){
 	var contactList = [];
 	var nextNumber = 0;
 	var alreadyCollected = false;
 	
 	return{
-	    initData : function() {
-	    	console.log("initData");	
+		/*
+		 * Go and get the content of the json file
+		 * and return a promise to work with.
+		 */
+	    initData : function() {	
 	        return $http({
 	            url: 'contacts.json.fix',
 	            method: 'GET'
 	        });
 	    },
+	    /*
+	     * Set the data so it can be shared.
+	     * Flag it has been collected so we don't reload
+	     * the data and loose its states.
+	     * Also go and find the next ID number to use
+	     */
 	    setData: function(data){
-	    	console.log("setData");
 	    	alreadyCollected = true;
 	    	contactList = data;
 	    	this.findNextNumber();
 	    },
+	    /*
+	     * Has the data already been collected?
+	     */
 	    isCollected: function(){
 	    	return alreadyCollected;
 	    },
+	    /*
+	     * Search through all the IDs of the contact and find the highest ID
+	     * then increment for use later on creation of another contact.
+	     * Ideally the ID would be a random value (perhaps a uuid?) so searching
+	     * wouldn't be required.
+	     */
 	    findNextNumber:function(){
 	    	var newNumber = 0;
-	    	console.log("startingNumber = 0");
 	    	for (var d = 0, len = contactList.length; d < len; d += 1) {
 	    		if(contactList[d].id >= newNumber){
 	    			newNumber = contactList[d].id;
-	    			console.log("newNumber now: " + newNumber);
 	    		}
 	    	}
 	    	newNumber++;
-	    	console.log("newNumber set at: " + newNumber);
 	    	nextNumber = newNumber;
 	    },
+	    /*
+	     * Get the next ID number that can be used.
+	     */
 	    getNextIDNumber: function(){
 	    	return nextNumber++;
 	    },
+	    /*
+	     * return the contact list
+	     */
 	    getData: function(){
-	    	console.log("getData");
 	    	return contactList;
 	    },
+	    /*
+	     * Get a specific contact details based on their ID.
+	     * A library or some sort of collection might be better than 
+	     * manually searching an array.
+	     * 
+	     */
 	    getContact: function(id){
-	    	console.log("Searching for ID: " + id);
 	    	for (var d = 0, len = contactList.length; d < len; d += 1) {
-	    		console.log("current id:" + contactList[d].id);
 	            if (contactList[d].id == id) {
-	            	console.log("Found it!");
 	                return contactList[d];
 	            }
 	        }
 	    },
+	    /*
+	     * Create a new contact with the given arguments and add
+	     * to the contact list array.
+	     * 
+	     * In retrospect this might be the best time to allocate the ID
+	     * and then return the ID used.
+	     */
 	    createContact: function(id, name, email, location, number){
 	    	var contact = {
 	    		id: id,
@@ -62,23 +106,28 @@ contactControllers.factory('Data', function($http){
 	    		primary: number
 	    	};
 	    	contactList.push(contact);
-	    	
 	    },
+	    /*
+	     * Update a given contact with the given arguments.
+	     * 
+	     * No check is done if the contact really exists, blind faith here.
+	     */
 	    updateContact: function(id, name, email, location, number){
-	    	console.log("going to update contact  id: ", id);
 	    	var contact = this.getContact(id);
-	    	console.log("was: " + contact.name + "now: " + name);
 	    	contact.name = name;
 	    	contact.email = email;
 	    	contact.location = location;
 	    	contact.primary = number;
 	    },
+	    /*
+	     * Delete a contact with a given ID.
+	     * 
+	     * Again, searching the array manually, a collection or library could
+	     * do this for me.
+	     */
 	    destroyContact: function(id){
-	    	console.log("going to destroy contact  id: ", id);
 	    	for (var d = 0, len = contactList.length; d < len; d += 1) {
-	    		console.log("current id:" + contactList[d].id);
 	            if (contactList[d].id == id) {
-	            	console.log("Found it del....!");
 	                contactList.splice(d,1);
 	                break;
 	            }
@@ -87,10 +136,23 @@ contactControllers.factory('Data', function($http){
 	 };
 });
 
-
+/*
+ * Create a controller for the contact list. It requires the global scope
+ * and the Data factory to be injected.
+ */
 contactControllers.controller('ContactListCtrl', ['$scope', 'Data',
 	function($scope, Data){
 	$scope.contacts = [];
+	/*
+	 * Since we flip between views, we need to retain the contact list state.
+	 * To do this we use a factory.  So we only need to initiate loading the
+	 * JSON file once, otherwise every time the controller is created we keep
+	 * loading the data again and lose the state. So....
+	 * Check if we have already collected the data,
+	 * If not then go and ask for it to be collected,
+	 * then on the success of collection set the data in the factory and store
+	 * this data in the scope.
+	 */
 	if(Data.isCollected()){
 		$scope.contacts = Data.getData();
 	}else{
@@ -99,9 +161,13 @@ contactControllers.controller('ContactListCtrl', ['$scope', 'Data',
 			$scope.contacts = data;	
 		});
 	}
+	/*
+	 * Default ordering of list. Can't we get the template to set the default?
+	 */
 	$scope.orderProp = 'id';
-	/**
-	 * TODO can we not move the destroy function by both controllers into another controller? so create a /delete/ route?
+	/*
+	 * When the delete button is pressed on the list, this is called. Which
+	 * calls the relevant factory method to do the deed.
 	 */
 	$scope.destroy = function(id){
     	Data.destroyContact(id);
@@ -109,22 +175,36 @@ contactControllers.controller('ContactListCtrl', ['$scope', 'Data',
 	
 }]);
 
+/*
+ * Create a controller for the detail view. Requires the following injections
+ * global scope, routeParams service to read the url,
+ * the location service to set the url and the Data factory.
+ * 
+ * This has no logic of dealing with an invalid contact ID, dangerous! 
+ */
 contactControllers.controller('ContactDetailCtrl', ['$scope', '$routeParams', '$location', 'Data',
 	function($scope, $routeParams, $location, Data){
+		/*
+		 * Get the contactID out of the url to go and retrieve the contact,
+		 * store the values in the global scope for the template to use
+		 */
 		$scope.contactID = $routeParams.contactID;
 		var contact = Data.getContact($scope.contactID);
-		console.log("Contact Found: " + contact);
-		console.log(contact);
 		$scope.contactName = contact.name;
 		$scope.contactEmail = contact.email;
 		$scope.contactLocation = contact.location;
 		$scope.contactNumber = contact.primary;
 		
+		/*
+		 * Update the contact with the values from the template. Then go back to the list view.
+		 */
 		$scope.save = function() {
 			Data.updateContact($scope.contactID,$scope.contactName,$scope.contactEmail,$scope.contactLocation,$scope.contactNumber);
 			$location.path('/');
 	    };
-	    
+	    /*
+	     * Delete the current contact then go back to the list view.
+	     */
 	    $scope.destroy = function(){
 	    	Data.destroyContact($scope.contactID);
 	    	$location.path('/');
@@ -132,16 +212,25 @@ contactControllers.controller('ContactDetailCtrl', ['$scope', '$routeParams', '$
 	}
 ]);
 
+/*
+ * Create a controller to handle creating a new contact.
+ * It requires the following injections,
+ * global scope, routeParams service to read the url,
+ * the location service to set the url and the Data factory.
+ */
 contactControllers.controller('ContactCreateCtrl', ['$scope', '$routeParams', '$location', 'Data',
 	function($scope, $routeParams, $location, Data){
+		/*
+		 * Get a new ID, call the create method in the factory and go back to the list.
+		 */
 		$scope.save = function() {
-			console.log("saving...");
 			$scope.contactID = Data.getNextIDNumber();
-			console.log("usign new id:" + $scope.contactID);
 			Data.createContact($scope.contactID,$scope.contactName,$scope.contactEmail,$scope.contactLocation,$scope.contactNumber);
 			$location.path('/');
 	    };
-	    
+	    /*
+	     * Go back to the list view 
+	     */
 	    $scope.destroy = function(){
 	    	$location.path('/');
 	    };
